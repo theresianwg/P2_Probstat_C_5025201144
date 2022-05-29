29e5fb64509b6f58da831caf1de933b5d69c20b8
theresianwg <81666422+theresianwg@users.noreply.github.com>
2022-05-29 23:51:17 +0700

    Update C_5025201144.R

--- C_5025201144.R
+++ C_5025201144.R
@@ -111,3 +111,40 @@ library("ggplot2")
 ggplot(my_data, aes(x = Group, y = Length)) + 
   geom_boxplot(fill = "white", colour = "black") + 
   scale_x_discrete() + xlab("Group") + ylab("Length")
+
+## No. 5
+library(readr)
+library(ggplot2)
+library(multcompView)
+library(dplyr)
+
+GTL <- read_csv("GTL.csv")
+head(GTL)
+#a 
+qplot(x = Temp, y = Light, geom = "point", data = GTL) +
+  facet_grid(.~Glass, labeller = label_both)
+
+#variabel untuk anova
+GTL$Glass <- as.factor(GTL$Glass)
+GTL$Temp_Factor <- as.factor(GTL$Temp)
+str(GTL)
+
+#b 
+anova <- aov(Light ~ Glass*Temp_Factor, data = GTL)
+summary(anova)
+
+#c
+data_summary <- group_by(GTL, Glass, Temp) %>%
+  summarise(mean=mean(Light), sd=sd(Light)) %>%
+  arrange(desc(mean))
+print(data_summary)
+
+#e
+print("Uji Tukey:")
+tukey <- TukeyHSD(anova)
+print(tukey)
+
+#f
+print("Compact Letter Display:")
+tukey.cld <- multcompLetters4(anova, tukey)
+print(tukey.cld)
